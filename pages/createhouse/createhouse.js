5058b7f630ee4f1b3bb37e1e53c7cbbad47baff8
hua1995116 <461249104@qq.com>
2018-03-04 22:18:39 +0800

    '完成创建逻辑'

--- pages/createhouse/createhouse.js
+++ pages/createhouse/createhouse.js
@@ -2,6 +2,54 @@ const app = getApp();
 
 Page({
   data: {
-    
-  }
+    location: '',
+    iphone: '',
+    date: '',
+    items: [
+      {name: 'yangtai', value: '阳台'},
+      {name: 'weishengjian', value: '独卫'},
+      {name: 'chufang', value: '厨房'},
+      {name: 'keting', value: '客厅'},
+      {name: 'xiyiji', value: '洗衣'},
+      {name: 'kongtiao', value: '空调'},
+      {name: 'bingxiang', value: '冰箱'},
+      {name: 'wuxian', value: '无线'},
+    ]
+  },
+  bindLocation: function() {
+    const _this = this;
+    wx.chooseLocation({
+      success: function (res) {
+        var name = res.name;
+        var address = res.address;
+        var latitude = res.latitude;
+        var longitude = res.longitude;
+        _this.setData({
+          location: name
+        })
+      }
+    })
+  },
+  bindDateChange: function(e) {
+    // console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  formSubmit: function(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value);
+    console.log(JSON.stringify(e.detail.value));
+    const formData = JSON.stringify(e.detail.value);
+    const data = wx.getStorageSync('create_list');
+    if(data) {
+      const list = JSON.parse(data);
+      list.push(formData);
+      wx.setStorageSync('create_list', JSON.stringify(list));
+      console.log(1);
+    } else {
+      const list = [];
+      list.push(formData);
+      wx.setStorageSync('create_list', JSON.stringify(list));
+    }
+  },
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -2,6 +2,19 @@ const app = getApp();
 
 Page({
   data: {
-    
+    listData: [],
+  },
+  onShow() {
+    let create_list = wx.getStorageSync('create_list');
+    if(create_list) {
+      const list = this.formatListData(JSON.parse(create_list));
+      console.log(list);
+      this.setData({
+        listData: list
+      })
+    }
+  },
+  formatListData(list) {
+    return list.map(item => JSON.parse(item))
   }
 })
